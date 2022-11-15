0d0750e1d577df50f50b30fa8c715e1c2c2fb550
Bharathi2110 <83777724+Bharathi2110@users.noreply.github.com>
2022-11-15 10:59:28 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 1/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__,template_folder='template')
+
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signin')
+def sigin():
+    return render_template('signin.html')
+
+@app.route('/about')
+def blog():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
